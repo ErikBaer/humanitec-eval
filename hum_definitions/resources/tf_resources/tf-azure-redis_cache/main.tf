7fe031bcce29131c9ca89cad632408802f822740
Erik Baer <baerdata.ger@googlemail.com>
2024-04-04 23:32:10 +0200

    add tf module fior azure redis cache

---
+++ hum_definitions/resources/tf_resources/tf-azure-redis_cache/main.tf
@@ -0,0 +1,40 @@
+variable resource_group_name {
+  type        = string
+  default     = ""
+  description = "description"
+}
+
+variable location {
+  type        = string
+  default     = ""
+  description = "description"
+}
+
+
+resource "azurerm_redis_cache" "example" {
+  name                = "example-redis-cache"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  capacity            = 1
+  family              = "C"
+  sku_name            = "Basic"
+  enable_non_ssl_port = false
+  minimum_tls_version = "1.2"
+
+  redis_configuration {
+    maxmemory_reserved = 2
+    maxmemory_delta    = 2
+    maxmemory_policy   = "allkeys-lru"
+  }
+}
+
+output "redis_cache_hostname" {
+  value = azurerm_redis_cache.example.hostname
+}
+
+output "redis_cache_primary_access_key" {
+  value = azurerm_redis_cache.example.primary_access_key
+  sensitive = true
+}
+
+
